36b589f3050768a3883f03930c16a70071adf413
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-15 15:16:46 -0300

    refactor: create UrlController

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const User = require('./models/User')
 
 const loginController = require('./controllers/LoginController')
 const signUpController = require('./controllers/SignUpController')
+const urlController = require('./controllers/UrlController')
 
 const authUseCase = require('./utils/AuthUseCase')
 
@@ -28,26 +29,6 @@ routes.get('/dashboard', async (req, res) => {
   }
 })
 
-routes.post('/addurl', async (req, res) => {
-  const authorizationHeader = req.headers.authorization
-
-  try {
-    const { isAuth, id, message } = await authUseCase.auth(authorizationHeader)
-
-    if (!isAuth) {
-      res.status(401).json({ message })
-    }
-
-    const data = req.body
-
-    const newURL = { original_url: data.url }
-
-    await User.updateOne({ _id: id }, { $push: { urls: newURL } })
-    res.status(200).json({ message: 'Sucess request' })
-  } catch (error) {
-    res.status(500).json({ message: 'Server error' })
-  }
-
-})
+routes.post('/addurl', urlController.add)
 
 module.exports = routes
