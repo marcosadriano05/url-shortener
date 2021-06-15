16fef75b3df37f391b77caab09889e9e4f042ab5
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-15 20:06:05 -0300

    Change variable name in CustomUrlController

--- src/controllers/CustomUrlController.js
+++ src/controllers/CustomUrlController.js
@@ -12,9 +12,9 @@ class CustomUrlController {
         return res.status(404).json({ message: 'Site not found' })
       }
 
-      const originalUrl = user.urls.find(url => url.shorted_url === customUrl)
+      const urls = user.urls.find(url => url.shorted_url === customUrl)
       
-      res.status(200).redirect(originalUrl.original_url)
+      res.status(200).redirect(urls.original_url)
     } catch (error) {
       res.status(404).json({ message: 'Site not found' })
     }
