fc4c292626eb035c63da02af3d1a211cde0b07b9
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-15 10:32:22 -0300

    Create addurl route

--- src/routes.js
+++ src/routes.js
@@ -62,4 +62,29 @@ routes.get('/dashboard', async (req, res) => {
   res.status(200).json(user)
 })
 
+routes.post('/addurl', async (req, res) => {
+  const authorizationHeader = req.headers.authorization
+  const token = authorizationHeader.split(' ')[1]
+
+  const { id } = jwt.verify(token, 'secret')
+  
+  const user = await User.findOne({ _id: id })
+
+  if (!user) {
+    res.status(401).json({ message: 'Unauthorized' })
+  }
+
+  const data = req.body
+
+  const newURL = { original_url: data.url }
+
+  try {
+    await User.updateOne({ _id: id }, { $push: { urls: newURL } })
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' })
+  }
+
+  res.status(200).json({ message: 'Sucess request' })
+})
+
 module.exports = routes
